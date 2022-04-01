9fcdb8f55ffbca2217877b896536610abf702f3b
Jason Wong <89881227+JasonW2021@users.noreply.github.com>
2022-04-02 12:41:51 +1300

    Creates buttons and check answer

--- multichoicequiz.py
+++ multichoicequiz.py
@@ -1,31 +1,40 @@
 #
 from tkinter import *
 
-
 class Multichoicequiz:
     def __init__(self, parent):
-    #Text = Question: What is the capital of Mongolia
-    #Possible - Vladivostok, Astana, Ulan Bator, Lhasa
-    #Incorrect / Correct label 
-        button= Radiobutton(parent, text="", variable="", value="")
-        button.grid(row=0, column=0, sticky =0)
+        '''Create the objects and radiobuttons'''
 
-        self.check = Label(parent, text = "",)
-        self.check.grid(column=0, row=0)
+        Questions = Label(parent, text = "Question: ")
+        Questions.grid(column= 0, row=0, sticky=W, columnspan=1)
+
+        Question1= Label(parent, text = "What is the capital of Mongolia?")
+        Question1.grid(column= 1, row=0)
+
+        self.check = Label(parent, text = "")
+        self.check.grid(column=1, row=5)
+
+        self.opt_list = ["Vladivostok", "Astana", "Ulan Bator", "Lhasa"]
+        self.opt_btns = []
+        self.opt_var = StringVar()
+        self.opt_var.set("*")
+
+        for i in range(len(self.opt_list)):
+            btns = Radiobutton(parent, text=self.opt_list[i], variable=self.opt_var,
+            anchor = W, value=self.opt_list[i], command= self.checkans)
+            btns.grid(column = 1, row = i+1, sticky=W)
+            self.opt_btns.append(btns)
 
     def checkans(self):
+        '''Check is radiobutton is correct or not'''
         answer = "Ulan Bator"
-        if self.check.get() == answer:
-            self.check.configure(text="correct")
+        if self.opt_var.get() == answer:
+            self.check.configure(text="Correct")
         else:
-            self.check.configure(text="incorrect")
-
-#columnspan
+            self.check.configure(text="Incorrect")
 
 #main routine
 root = Tk()
-opt_list = ["Vladivostok", "Astana", "Ulan Bator", "Lhasa"]
-options = StringVar()
-options 
+root.geometry("300x200")
 multiquestion = Multichoicequiz(root)
 root.mainloop()
